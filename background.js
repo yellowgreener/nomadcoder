2170a4492c815e7ebf8cf97d28e7f64a1cbcf87c
yellowgreener <69282138+yellowgreener@users.noreply.github.com>
2021-02-16 00:04:23 +0900

    fixed for github.io
    github.io 에서 사용하기 위해 이미지 소스 코드 변경

--- background.js
+++ background.js
@@ -4,6 +4,8 @@ const IMG_NUMBER = 5;
 
 function paintImage(imgNumber){
     const image = new Image();
+    // image.src = `/images/vincentVanGogh${imgNumber + 1}.jpg`;
+    // github.io 에서 사용하기 위해 코드 변경
     image.src = `https://yellowgreener.github.io/nomadcoder/images/vincentVanGogh${imgNumber + 1}.jpg`;
     image.classList.add("bgImage");
     body.appendChild(image);
