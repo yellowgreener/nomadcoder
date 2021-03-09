3bf54725ed6aa70a08b0e7e06aaa4552b4a551e2
yellowgreener <kangsujin@kakao.com>
2021-03-09 14:49:31 +0900

    수정_기능추가
    이름변경 기능추가

--- greeting.js
+++ greeting.js
@@ -1,9 +1,17 @@
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
-    greeting = document.querySelector(".js-greetings");
+    greeting = document.querySelector(".js-greetings"),
+    changeNameBtn = document.querySelector(".changeName");
 
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
+
+function changeName(event){
+    event.preventDefault();
+    let changeValue = prompt("변경하실 이름을 입력하세요");
+    paintGreeting(changeValue);
+    saveName(changeValue);
+}
 
 function saveName(text){
     localStorage.setItem(USER_LS, text);
@@ -11,7 +19,7 @@ function saveName(text){
 
 function handleSubmit(event){
     event.preventDefault();                 // 기본 submit 이벤트 중지
-    const currentValue = input.value;
+    let currentValue = input.value;
     paintGreeting(currentValue);
     saveName(currentValue);                 // 새로고침해도 저장된 값을 기억한다.
 }
@@ -24,6 +32,7 @@ function askForName(){
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
+    changeNameBtn.setAttribute('style', 'display:block');
     greeting.innerText = `${text}님 반가워요!!!`;
 }
 
@@ -38,6 +47,7 @@ function loadName(){
 
 function init(){
     loadName();
+    changeNameBtn.addEventListener("click", changeName);
 }
 
 init();
